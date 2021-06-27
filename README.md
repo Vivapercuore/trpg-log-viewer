d89071b1ee0833be36080a3d0d90c8460b367b71
vivapercuore <Vivapercuore@aliyun.com>
2021-06-28 01:30:46 +0800

    feat: readMe & project config

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-
 # 跑团记录生成器
 
 将跑团日志文件转换为可视化跑团记录
 
 # 访问地址
+
 暂未上线
+
+# 帮助文档
+
+https://vivapercuore.github.io/trpg-log-viewer/
 
 # 项目开发：
 
 使用 Vue 3 + Typescript + Vite 开发
+
 ## How to Run
 
 ```
--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,17 @@
 import path from "path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    styleImport({
-      libs: [{
-        libraryName: 'element-plus',
-        esModule: true,
-        resolveStyle: (name) => {
-          return `element-plus/lib/theme-chalk/${name}.css`;
-        },
-        resolveComponent: (name) => {
-          return `element-plus/lib/${name}`;
-        },
-      }]
-    }),
   ],
-  resolve:{
-    alias: {
-        'src': path.resolve(__dirname, 'src')
-    },
-  },
+	resolve: {
+		alias: [
+			{ find: '@src', replacement: path.resolve(__dirname, 'src') },
+			{ find: '@img', replacement: path.resolve(__dirname, 'src/assets') },
+		],
+	},
 })
 

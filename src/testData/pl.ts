1c0e03916b353655b17f7f7f5ad92a0763c9f1af
vivapercuore <Vivapercuore@aliyun.com>
2021-07-10 23:47:27 +0800

    feat: add Tachie

--- src/testData/pl.ts
+++ src/testData/pl.ts
@@ -7,10 +7,12 @@ const defaultVoice {
 
 export class roles {
     voice:VoiceType
+    tachies:[Tachie]
     constructor(){
         this.voice = defaultVoice
+        this.tachies = []
     }
-
+    
 }
 
 interface VoiceType {
@@ -18,4 +20,11 @@ interface VoiceType {
     pit: number ；  //选填	音调，取值0-15，默认为5中语调
     vol: number ； //选填	音量，取值0-15，默认为5中音量
     per: number ； //选填	发音人选择, 0为普通女声，1为普通男生，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女声
+}
+
+
+interface Tachie {
+    img: string;
+    position: position;
+    size: size;
 }
